dee342c6a54128b0157238e308779c95fb515050
Gavin Fowler <gavins.username@gmail.com>
2019-09-27 12:48:50 -0600

    rebuilding site Fri Sep 27 12:48:50 MDT 2019

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,15 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo -t ezhil
+hugo -t hyde
 
-# Go To Public folder
-cd public
+# Commit changes to theme
+cd themes/hyde
+git add .
+git commit -m "updating theme `date`"
+git push origin/master
+cd ../..
+
 # Add changes to git.
 git add .
 
@@ -20,9 +25,6 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-
-# Come Back up to the Project Root
-cd ..
 
 # Trigger netlify build
 curl -X POST -d {} https://api.netlify.com/build_hooks/5d7bf76d8d171b0181af0a8b
